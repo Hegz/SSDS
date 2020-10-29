e35695f13b4459bc57f88c052ee64c8cb9e59b86
Adam Fairbrother <adam.fairbrother@gmail.com>
2020-10-29 12:51:12 -0700

    fixing some basic update awareness

--- wrapper.sh
+++ wrapper.sh
@@ -3,5 +3,6 @@
 cd /home/ubuntu
 
 while true; do
+	echo === Starting presentation ====
 	/home/ubuntu/presentation.sh 2>&1 | while read line; do echo [$(date)] - $line; done | tee -a $(date -I).txt
 done
