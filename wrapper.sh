adffc9352e2bf6798639dd1034de087730055a59
Adam Fairbrother <adam.fairbrother@gmail.com>
2023-10-20 14:47:22 -0700

    Disabled log file recording.

--- wrapper.sh
+++ wrapper.sh
@@ -4,5 +4,5 @@ cd /home/otto
 
 while true; do
 	echo === Starting presentation ====
-	/home/otto/ssds/presentation.sh 2>&1 | while read line; do echo [$(date)] - $line; done | tee -a log/$(date -I).txt
+	/home/otto/ssds/presentation.sh 2>&1 /dev/null
 done
