7b3120981d8733ea767488103a29d7bff7bae8f7
Adam Fairbrother <adam.fairbrother@gmail.com>
2026-07-09 11:40:55 -0700

    Added logging to wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -2,7 +2,26 @@
 
 cd /home/otto
 
+# Helper function to log wrapper status cleanly to journalctl
+function log_wrapper() {
+    local priority="$1"
+    local message="$2"
+    logger -t presentation-wrapper -p "user.$priority" "$message"
+    echo "[$priority] $message"
+}
+
+log_wrapper notice "Presentation wrapper service started."
+
 while true; do
-	# echo === Starting presentation ====
-	/home/otto/ssds/presentation.sh > /dev/null 2>&1
+    log_wrapper info "Starting presentation script execution loop..."
+    
+    # Run the presentation script, capturing stdout/stderr and routing to logger
+    # This ensures anything the main script misses is still caught!
+    /home/otto/ssds/presentation.sh 2>&1 | logger -t presentation-script -p user.notice
+
+    # If the script finishes or crashes, catch the exit code
+    EXIT_CODE=${PIPESTATUS[0]}
+    
+    log_wrapper warning "Presentation script exited unexpectedly with code $EXIT_CODE. Restarting in 3 seconds..."
+    sleep 3
 done
